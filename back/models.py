a61f429c55491fd3ea745ea85886c62ac98be753
euj4ckson <jacksonduardo6@gmail.com>
2024-12-11 11:25:12 -0300

    models update

--- back/models.py
+++ back/models.py
@@ -17,23 +17,6 @@ class Usuario(db.Model):
     def __repr__(self):
         return f'<Usuario {self.nome}>'
 
-
-class Evento(db.Model):
-    __tablename__ = 'eventos'
-
-    id = db.Column(db.Integer, primary_key=True)
-    titulo = db.Column(db.String(200), nullable=False)
-    descricao = db.Column(db.Text, nullable=False)
-    usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
-    criado_em = db.Column(db.DateTime, default=db.func.current_timestamp())
-    atualizado_em = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
-
-    # Define o relacionamento com Usuario usando back_populates para evitar conflito
-    usuario = db.relationship("Usuario", back_populates="eventos")
-
-    def __repr__(self):
-        return f'<Evento {self.titulo}>'
-
 class Produto(db.Model):
     __tablename__ = 'produtos'
 
