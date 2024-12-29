d313b7dfb6dd9975219fad8e83df52fc7aa22fcc
viniciusmachadovianna <viniciusmachadovianna@outlook.com>
2024-12-29 20:40:19 -0300

    products manually added in menu li

--- front/script.js
+++ front/script.js
@@ -8,11 +8,30 @@ function resize(element,width,height=null){
     if(height)element.style.height=`${height}px`
 }
 function toggleSize(element){
-    if(attr(menu,'data-size')==='opened'){console.log('is open')}
-    else console.log(('closed'));
-    //not passing e.target properly
+    const sizes = {
+        search: {
+            minWidth: 50,
+            minHeight: 50,
+            maxWidth: 300,
+            maxHeight: 50,
+        },
+        menu: {
+            minWidth: 50,
+            minHeight: 50,
+            maxWidth: 300,
+            maxHeight: 300,
+        }
+    }
+    let el = 'menu';
+    console.log(sizes[el].);
+    // let a = parseFloat(window.getComputedStyle(menu).width);
+    // console.log(sizes[el].minHeight);
+    console.log(parseFloat(window.getComputedStyle(menu).width));
+    console.log(menu);
+    // if(attr(menu,'width')==='opened'){console.log('is open')}
+    // else console.log(('closed'));
 }
 console.log(menu, menu.getAttribute('data-size'));
 menu.setAttribute('data-size','closed');
-menu.addEventListener('click',(e)=>toggleSize(e.target))
+menu.addEventListener('click',(e)=>toggleSize(menu))
 search.addEventListener('click',()=>resize(search,100))
