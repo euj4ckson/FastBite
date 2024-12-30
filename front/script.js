e53e30005a0a57ada36931680a7b0497292e9469
Vinícius Machado <viniciusmachadovianna@outlook.com>
2024-12-30 17:47:19 -0300

    toast color+msg
    filters manually added at page bottom

--- front/script.js
+++ front/script.js
@@ -1,17 +1,27 @@
 const $=(element)=>document.querySelector(element),
     getId=(element)=>document.getElementById(element),
     attr=(element,attribute)=>element.getAttribute(attribute),
-    _ = null,
-    menu = $('nav'),
-    search = $('search'),
-    btnAdd = $('.add2OrderBtn'),
-    orders = getId('ordersBtn'),
-    toast = getId('toast')
-function resizeElementElement(element,width,height=null){
+    _=null,
+    menu=getId('burgerIcon'),
+    search=$('search'),
+    btnAdd=$('.add2OrderBtn'),
+    orders=getId('ordersBtn'),
+    toast=getId('toast'),
+    filters=getId('quickFilters').querySelectorAll('#pizzas,#burgers,#beverages')
+function resizeElement(element,width,height=null){
     element.style.width=`${width}px`
     if(height)element.style.height=`${height}px`
 }
-
+function setUIcolor(state='standard'){
+    const colors={
+        standard:'white',
+        filter:'yellow',
+        success:'green',
+        error:'red'
+    }
+    document.querySelectorAll('body',menu,search).forEach((el)=>el.style.background=colors[state])
+    // document.body.style.background
+}
 function toggleVisibility(element){
     // console.log(
         // document.querySelectorAll('menu,search')
@@ -48,11 +58,14 @@ function changeSize(element){
 }
 function createOrder(){
     console.log('create')
-    showToast('saved','success')
+    showToast('saved','info')
 }
 function editOrder(){
-    console.log('edit');
+    // console.warn('edit');
+    // console.error('edit');
+    // console.info('edit');
 }
+editOrder();
 function deleteOrder(orderId){
     console.log('delete');
 }
@@ -62,16 +75,22 @@ function getOrdersList(){
 function getOrderDetails(){
     console.log('1 order details');
 }
-function showToast(msg,type=null){
-    // Standard: Blue - INFO
-    const state = {
-        info:'lightblue',
-        success: 'green',
-        warning: 'yellow',
-        error: 'red',
+function showToast(msg,type=info){
+    const states = {
+        info:'#d4f4ff',
+        infoBorder:'#00bbff',
+        success:'#ccffd5',
+        successBorder:'#007526',
+        warning:'#ffef76',
+        warningBorder:'#ffe100',
+        error:'#ff7b76',
+        errorBorder:'#ff0800',
     }
+    resizeElement(toast,200);
+    // Standard: Blue - INFO
     toast.style.display='flex';
-    toast.style.background=!type?state.info:state[type];
+    toast.style.background=states[type];
+    toast.style.borderColor=states[`${type}Border`];
     toast.textContent=msg
 }
 // console.log(menu, menu.getAttribute('data-size'));
@@ -80,3 +99,4 @@ menu.addEventListener('click',(e)=>changeSize(menu))
 // search.addEventListener('click',()=>resizeElement(search,100))
 orders.addEventListener('click',()=>console.log(createOrder()))
 btnAdd.addEventListener('click',()=>console.log(btnAdd))
+filters.forEach((filter)=>{filter.addEventListener('click',(e)=>{setUIcolor()})})
